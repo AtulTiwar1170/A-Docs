1ccea27100d81aa415958eada4176ab60829c46c
AtulTiwar1170 <atult2620@gmail.com>
2024-08-21 11:08:20 +0530

    completed

--- Docss/src/TextEditor.jsx
+++ Docss/src/TextEditor.jsx
@@ -4,15 +4,17 @@ import "quill/dist/quill.snow.css";
 import "./Editor.css"
 import { io } from "socket.io-client";
 import { useParams } from 'react-router-dom';
+import { saveAs } from 'file-saver';
+
 
 const SAVE_INTERVAL_MS = 2000;
 const TOOLBAR_OPTION = [
     [{ header: [1, 2, 3, 4, 5, 6, false] }],
-    [{ font: []}],
+    [{ font: [] }],
     [{ list: "ordered" }, { list: "bullet" }],
     ["bold", "italic", "underline"],
     [{ color: [] }, { background: [] }],
-    [{ script: "sub"}, { script: "super"}],
+    [{ script: "sub" }, { script: "super" }],
     [{ align: [] }],
     ["image", "blockquote", "code-block"],
     ["clean"],
@@ -22,6 +24,10 @@ const TextEditor = () => {
     const { id: documentId } = useParams()
     const [socket, setSocket] = useState();
     const [quill, setQuill] = useState();
+    const [expiryTime, setExpiryTime] = useState(0);
+
+
+
 
     useEffect(() => {
         const s = io("http://localhost:3000")
@@ -31,7 +37,7 @@ const TextEditor = () => {
         }
     }, [])
 
-    useEffect( () => {
+    useEffect(() => {
         if (socket == null || quill == null) return
         socket.once("load-document", document => {
             quill.setContents(document)
@@ -41,7 +47,7 @@ const TextEditor = () => {
     }, [socket, quill, documentId])
 
     useEffect(() => {
-        if (socket == null || quill == null) return 
+        if (socket == null || quill == null) return
         const handler = (delta) => {
             quill.updateContents(delta)
         }
@@ -51,20 +57,20 @@ const TextEditor = () => {
         }
     }, [socket, quill])
 
-    useEffect( () => {
+    useEffect(() => {
         if (socket == null || quill == null) return
-        
-        const  interval = setInterval(() => {
+
+        const interval = setInterval(() => {
             socket.emit("save-document", quill.getContents());
-        },  SAVE_INTERVAL_MS);
+        }, SAVE_INTERVAL_MS);
         return () => clearInterval(interval);
     }, [socket, quill])
 
 
     useEffect(() => {
-        if (socket == null || quill == null) return 
+        if (socket == null || quill == null) return
         const handler = (delta, oldDelta, source) => {
-            if (source !== 'user') return 
+            if (source !== 'user') return
             socket.emit('send-changes', delta)
         }
         quill.on('text-change', handler);
@@ -75,11 +81,11 @@ const TextEditor = () => {
 
 
     const wrapperRef = useCallback(wrapper => {
-        if(wrapper == null) return
+        if (wrapper == null) return
         wrapper.innerHTML = ""
         const textEditor = document.createElement("div")
-        wrapper.append(textEditor) 
-        const q = new Quill( textEditor , {
+        wrapper.append(textEditor)
+        const q = new Quill(textEditor, {
             theme: "snow",
             modules: { toolbar: TOOLBAR_OPTION },
         })
@@ -87,7 +93,51 @@ const TextEditor = () => {
         q.setText("Loading...")
         setQuill(q);
     }, []);
-    return <div className="container" ref={wrapperRef}></div>
+
+    const handleDownload = () => {
+        const data = quill.editor.delta.ops['0'].insert;
+        const blob = new Blob([data], { type: 'text/plain' });
+        saveAs(blob, 'documents.docs');
+    }
+    const handleShare = () => {
+        setExpiryTime(new Date().getTime() + 60 * 60 * 1000)
+
+        const genirateTocken = () => {
+            const token = btoa(JSON.stringify({ exp: expiryTime }));
+            return token;
+        }
+        const createShareLink = () => {
+            const token = genirateTocken();
+            const link = `${window.location.href}?token=${token}`;
+            return link;
+            
+        }
+        if (navigator.share) {
+            const shareLink = createShareLink()
+            navigator.share({
+                url: shareLink,
+            })
+                .then(() => console.log('Successful share'))
+                .catch((error) => console.error('Error sharing:', error));
+        } else {
+            console.log('Web Share API not supported');
+        }
+    };
+    const printPage = () => {
+        window.print();
+    }
+
+    return (
+        <>
+            <div className="container" ref={wrapperRef}></div>
+            <div className='download'>
+                <button className='btn' onClick={handleDownload} >Save</button>
+                <button className='btn' onClick={handleShare} >Give access</button>
+                <button className='btn' onClick={printPage} >Print</button>
+            </div>
+        </>
+
+    )
 }
 
 export default TextEditor
